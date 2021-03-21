d7003bf81e932f1e74fadccb1b738f82cb91a23d
vall-valentine <vallvalentine77@yandex.ru>
2021-03-21 12:44:20 +0300

    Добавлены данные в бд

--- data/__all_models.py
+++ data/__all_models.py
@@ -6,8 +6,8 @@ from .db_session import SqlAlchemyBase
 class Temp(SqlAlchemyBase):
     __tablename__ = 'data'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer,
+    id = sqlalchemy.Column(sqlalchemy.BIGINT,
                            primary_key=True, autoincrement=True)
     date = sqlalchemy.Column(sqlalchemy.TIMESTAMP, nullable=True)
-    temp = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    city = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    temp = sqlalchemy.Column(sqlalchemy.FLOAT, nullable=True)
+    city = sqlalchemy.Column(sqlalchemy.TEXT, nullable=True)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 import pandas as pd
-
+from sqlalchemy import create_engine
+from data import __all_models
+from data import db_session
 
 app = Flask(__name__)
 
@@ -22,43 +24,5 @@ def cur_city():
     return render_template('cur_city.html')
 
 
-@app.route('/db', methods=['GET'])
-def form():
-    # Названия файлов
-    files = ['Алмазный.json', 'Западный.json', 'Курортный.json', 'Лесной.json', 'Научный.json',
-             'Полярный.json', 'Портовый.json', 'Приморский.json', 'Садовый.json', 'Северный.json',
-             'Степной.json', 'Таежный.json', 'Южный.json']
-
-    # Считываем все файлы и собираем в 1 Dataframe
-    dfs = []
-    for file in files:
-        city = file[:-5]
-        df = pd.read_json(pd.read_json('data/' + file))
-        df.columns = [city]
-        dfs.append(df)
-
-    temp_df = pd.concat(dfs, axis=1)
-    temp_df = temp_df.reset_index(drop=True)
-
-    # Генерируем даты (года 2000-2019)
-    dates = pd.date_range(start='2000-01-01', periods=7305, freq='D')
-    # Исключаем високосные "дни"
-    leap_days = (dates.month == 2) & (dates.day == 29)
-    dates = dates[~leap_days]
-
-    # Добавим индекс по времени
-    temp_df.index = dates
-
-    for temp in temp_df:
-        print(temp)
-
-    return render_template('main_page.html')
-
-
 if __name__ == '__main__':
-    db_session.global_init("db/database.sqlite")
-    session = db_session.create_session()
-
-
-
     app.run(port=8080, host='127.0.0.1')
