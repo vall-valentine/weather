301b09cc8a2c1710f935cd8756106395d5db82f8
vall-valentine <vallvalentine77@yandex.ru>
2021-03-21 13:33:54 +0300

    q

--- forms/forms.py
+++ forms/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import DateField
+from wtforms import StringField
 from wtforms.validators import DataRequired
 
 
 class Form(FlaskForm):
-    data1 = DateField('Login', validators=[DataRequired()])
-    data2 = DateField('Password', validators=[DataRequired()])
+    data1 = StringField('Login', validators=[DataRequired()])
+    data2 = StringField('Password', validators=[DataRequired()])
--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from sqlalchemy import create_engine
 from data.__all_models import Temp
 from data import db_session
 from forms.forms import Form
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'solnyshki_vperyod'
 
 
 # все города
@@ -21,30 +23,27 @@ def cur_city():
     db_session.global_init("db/database.sqlite")
     session = db_session.create_session()
 
-    return render_template('cur_city.html')
-
-
-# график погоды
-@app.route('/cities/<city>', methods=['GET'])
-def cur_city():
-    db_session.global_init("db/database.sqlite")
-    return render_template('cur_city.html')
+    return render_template('grath.html')
 
 
 # прогноз погоды
-@app.route('/cities/<city>/predicts', methods=['GET'])
-def cur_city(city):
+@app.route('/cities/<city>/predicts', methods=['GET', 'POST'])
+def cur_city_pred(city):
     db_session.global_init("db/database.sqlite")
     session = db_session.create_session()
-    form = Form
+    form = Form()
     if form.is_submitted():
-        d1 = session.query(Temp).filter(Temp.data == form.data1.data, Temp.city == city).first()
-        d2 = session.query(Temp).filter(Temp.data == form.data1.data, Temp.city == city).first()
+        q= session.query(Temp).filter(Temp.id == 1).first()
+        print(q.date == datetime('2020-01-01 00:00:00.000000'))
+
+        d1 = session.query(Temp).filter(Temp.date == form.data1.data, Temp.city == city).first()
+        d2 = session.query(Temp).filter(Temp.date == form.data1.data, Temp.city == city).first()
         temps_data = []
         for i in range(d1.id, d2.id):
+
             a = session.query(Temp).filter(Temp.id == i).first()
             temps_data.append(tuple(a.id, a.date))
-        return render_template('forecast.html', temps_data=temps_data, city=city)
+        return render_template('forecast.html', form=form, temps_data=temps_data, city=city)
     return render_template('forecast.html', form=form)
 
 
