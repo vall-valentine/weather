f3dab688b5b60ceb350c5c809f1f17f39f1e9050
olskalim <is.2003.01@gmail.com>
2021-03-21 14:10:15 +0300

    написана консольная утилита

--- plot.py
+++ plot.py
@@ -7,9 +7,12 @@ def filter(path):
     file = open(path)
     data = json.load(file)
     file.close()
+    print("Ошибки в файле:")
+    print("день | ошибка")
     for i in range(1, len(data)-1):
         if abs((data[i-1]+data[i+1])/2-data[i]) > 10:
             data[i] = (data[i-1]+data[i+1])/2
+            print(i, data[i])
     return data
 
 
@@ -33,9 +36,9 @@ def genAver(arr):
     return data
 
 
-def makePlot(f,a):
+def makePlot(f, a, b):
     filtred = filter(f)
-    plt.plot([m for m in range(0, a)], genAver(filtred))  # Ox, Oy
+    plt.plot([m for m in range(a, b)], genAver(filtred)[a:b])  # Ox, Oy
     plt.show()
 
 
@@ -49,5 +52,48 @@ def writeFile():
     t = str(makePredictArr(r"data\20_years_format\Южный.json"))
     file.write(t)
 
+
+def prognoz(f, a, b):
+    file = open(f)
+    data = json.load(file)
+    file.close()
+    print("день | температура")
+    for i in range(a, b):
+        print(i, data[i], sep="     ")
+    print("Построить график по этому промежутку?")
+    if input("(да / нет): ") == "да":
+        makePlot(f, a, b)
+
+
+# первый параметр - файл по которому строится график
 # второй параметр для года 365, для всего промежутка 7300
-makePlot(r"data\20_years_format\Южный.json", 7300)
+# makePlot(r"data\20_years_format\Южный.json", 7300)
+
+def main():
+    print("Вы хотите построить график по историческим данным или посмотреть прогноз?")
+    t = input("(график / прогноз): ")
+    if t == "график":
+        addr = input("Ведите название города для построения графика: ")
+        print("построить график за год или за весь период измерений?")
+        t = input("(год / период): ")
+        if t == "период":
+            a = 0
+            b = 7300
+        elif t == "год":
+            a = (int(input("Введите начальный год (от 1 до 20): "))-1)*365
+            b = a+365
+        addr = str("data/") + str(addr) + str(".json")
+        makePlot(addr, a, b)
+    elif t == "прогноз":
+        addr = input("Ведите название города: ")
+        a = int(input("Введите первый день интервала (>= 0): "))
+        b = int(input("Введите последний день интервала (<= 365): "))
+        addr = str("data/predictions/") + str(addr) + str("0.json")
+        prognoz(addr, a, b)
+    else:
+        print("Неправильный формат ввода.")
+    print()
+
+
+while True:
+    main()
